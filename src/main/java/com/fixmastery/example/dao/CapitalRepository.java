42e69f227f758e16840bbd393dcbc631a3eb695d
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2020-05-22 12:00:38 -0400

    Deleted example components

--- src/main/java/com/fixmastery/example/dao/CapitalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fixmastery.example.dao;
-
-import com.fixmastery.example.model.Capital;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CapitalRepository extends CrudRepository<Capital, String>, JpaSpecificationExecutor<Capital> {}
